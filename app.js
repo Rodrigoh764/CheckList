f3e33718d701e1944eae8736575a432beea7dace
Rodrigo <86635430+Rodrigoh764@users.noreply.github.com>
2023-03-14 12:28:27 -0600

    Programa finalizado, se creo una carpeta y dentro se crearon dos archivos para modularizar el programa, se reduccio el codigo

--- app.js
+++ app.js
@@ -1,100 +1,30 @@
-(() => {
-const btn = document.querySelector("[data-form-btn]");
+import checkComplete from './components/checkComplete.js';
+import deleteIcon from './components/deleteIcon.js';
+
+const btn = document.querySelector('[data-form-btn]');
 
 const createTask = (evento) => {
-  //ESTO ES PARA QUE LA PAGINA NO SE ESTE REFRESCANDO A CADA RATO
-  //ESTO AYUDA A QUE NO SE ENVIEN DATOS A UN LADO
   evento.preventDefault();
-
-  //OBTENEMOS ACESO AL CAMPO
-  const input = document.querySelector("[data-form-input]");
-
-  //OBTENEMOS EL VALOR DEL CAMPO
+  const input = document.querySelector('[data-form-input]');
   const value = input.value;
+  const list = document.querySelector('[data-list]');
+  const task = document.createElement('li');
+  task.classList.add('card');
+  input.value = '';
+  //backticks
+  const taskContent = document.createElement('div');
 
-  //OBTENEMOS EL ACESO AL CAMPO
-  const list = document.querySelector("[data-list]");
-
-  //INDICAMOS QUE NOS CREE ELEMETOS DE TIPO IL
-  const task = document.createElement("li");
-
-  //INDICAMOS QUE SE HARA UNA LISTA SUMANDO LOS ELEMENTOS
-  task.classList.add("card");
-
-  //CON ESTA ACCION SE LOGRA BORRAR LO QUE SE INGRESO EN AGREGAR TAREA
-  input.value = "";
-
-
-  const taskContent = document.createElement("div");
-
-
-
-  const titleTask = document.createElement("span");
-  titleTask.classList.add("task");
+  const titleTask = document.createElement('span');
+  titleTask.classList.add('task');
   titleTask.innerText = value;
   taskContent.appendChild(checkComplete());
-  taskContent.appendChild(titleTask)
+  taskContent.appendChild(titleTask);
+  // task.innerHTML = content;
+
   task.appendChild(taskContent);
+  task.appendChild(deleteIcon());
   list.appendChild(task);
-
-
-//NO AGARRA
-
-
-  
 };
 
-btn.addEventListener("click", createTask);
-
-const checkComplete = () => {
-  const i = document.createElement("i");
-  i.classList.add("far", "fa-check-square", "icon");
-  i.addEventListener("click", completeTask);
-  return i;
-};
-
-const completeTask = (event) => {
-  /*
-  const element = event.target;
-  element.classList.add("fas");
-  element.classList.add("completeIcon");
-  element.classList.remove("far");
-  
-  EL TOGGLE LO QUE HACE ES QUE SI EXISTE LA CLASE LO QUITO Y SI NO EXISTE LA PONGO
-  ES UN COMODIN YA QUE ARRIBA LO QUE TENEMOS ES ADD QUE ES SUMAR O REMOVE QUE ES ELIMINAR
-
-  con el elemento event.target podemos capturar el elemento que dio click, en este caso lo guardamos en element
-
-------------------------------------------------------
-Alternativa Correcta. ¡Exacto! A través de la variable botaoConclui capturamos en cual elemento fue hecho click, 
-después utilizamos la propiedad parentElement para subir un nodo en el árbol, y por fin, 
-pusimos el método toggle que adicionará la clase done cuando hagamos click en el botón
-
-const botaoConclui = evento.target
-
-const tarefaCompleta = botaoConclui.parentElement
-
-tarefaCompleta.classList.toggle('done')
---------------------------------------------------
-  */
-  const element = event.target;
-  element.classList.toggle("fas");
-  element.classList.toggle("completeIcon");//ESTA CLASE ES DE CSS, PODEMOS HACER USO DE LAS CLASES DE CSS
-  element.classList.toggle("far");
-  
-   
-}
-
-const deleteIcon = () =>{
-
-const i = document.createElement("i");
-i.classList.add('fas', 'fa-trash-alt', 'trashIcon', 'icon');
-i.addEventListener("click", deleTask);
-return i;
-}
-const deleTask = () =>{
-  console.log("Eliminar");
-}
-
-}) //ESTOS PARENTESIS QUE ACABAMOS DE COLOCAR ES ALGO QUE SE LLAMA EN JAVASCRIP IIFE, QUE HACE QUE LAS FUNCIONES
-//QUE ESTAN DENTRO DEL ARCHIVO NO PUEDAN SER INVOCADAS A TRAVES DE LA CONSOLA DEL NAVEGADOR
+//Arrow functions o funciones anonimas
+btn.addEventListener('click', createTask);
